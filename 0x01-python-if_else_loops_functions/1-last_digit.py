54738c12c15acf17308bb38018e1568f22b41b74
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-06-01 12:45:40 +0100

    SPELLING

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
 else:
     number1 = number % 10
 if number1 > 5:
-    print('last digit of {} is {} and is greater than 5'.format(number, number1))
+    print('Last digit of {} is {} and is greater than 5'.format(number, number1))
 elif number1 < 6:
-    print('last digit of {} is {} and is less than 6 and not 0'.format(number, number1))
+    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, number1))
 elif number1 == 0:
-    print('last digit of {} is {} and is 0'.format(number, number1))
+    print('Last digit of {} is {} and is 0'.format(number, number1))

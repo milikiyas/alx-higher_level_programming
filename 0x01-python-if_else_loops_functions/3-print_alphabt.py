da944a9944e1e2c41614e94c205bda7f58bb6168
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-06-01 19:28:54 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,8 @@
-
+k = ord('a')
+while k <= 122:
+    if not(k == 101 or k == 113):
+        ch = chr(k)
+        print('{}'.format(ch), end="")
+        k += 1
+    elif k == 101 or k == 113:
+        k += 1

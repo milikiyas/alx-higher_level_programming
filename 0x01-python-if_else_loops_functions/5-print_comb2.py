2babd09c4265db9aaf57b7c688767a704a05ef92
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-06-01 21:08:51 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,8 @@
 k = 0
 while k <= 99:
  if k <= 98:
-  print('{:02}'.format(k), end=", ")
-  k += 1
+    print('{:02}'.format(k), end=", ")
+    k += 1
  else:
     print('{:02}'.format(k))
     k += 1

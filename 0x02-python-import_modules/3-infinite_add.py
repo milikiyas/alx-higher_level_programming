11556b764d88998ddc6f8a827354e128ceb749b1
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-06-02 20:45:34 +0100

    Create 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print('0')
+    else:
+        sum = 0
+        for i in range(count):
+            sum += int(sys.argv[i + 1])
+        print(sum)

c9550522bf178e511ad9a9ac50e11e8a18b5535c
milikiyaswosenseged <99360267+milikiyas@users.noreply.github.com>
2022-06-11 07:27:25 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
